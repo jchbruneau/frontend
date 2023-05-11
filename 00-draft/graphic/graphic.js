676158b08f59a7b1331c724be47079033e340527
jchbruneau <jch.bruneau@gmail.com>
2023-05-11 16:06:37 +0200

    Update graphics to make it dynamic

--- 00-draft/graphic/graphic.js
+++ 00-draft/graphic/graphic.js
@@ -1,5 +1,6 @@
 const file = 'data.json';
 
+let id = 0;
 let objects = new Array();
 
 window.addEventListener('DOMContentLoaded', (event) => {
@@ -13,14 +14,15 @@ function loadObject(json) {
     objects = new Array();
     const box = document.getElementsByClassName('box')[0];
     for (let i = 0; i < json.graphics.length; i++) {
+        id = i+1;
         addObject(json.graphics[i]);
-        code += displayGraphic(i+1,objects[i], 10, 5);
+        code += displayGraphic(10, 5);
     }
     box.innerHTML = code;
 }
 
 function addObject(graphic) {
-    const object = { type: '', title: '', min: {}, max: {}, data: [] }
+    const object = { type: '', title: '', min: {}, max: {}, length: {}, data: [] }
     object.type = graphic.type;
     object.title = graphic.title;
     object.data = graphic.data;
@@ -34,30 +36,39 @@ function addObject(graphic) {
             if (object.max.y === undefined || object.max.y < graphic.data[i].y) { object.max.y = graphic.data[i].y; }
         }
     }
+    fixValues(object);
     objects.push(object);
 }
 
-function displayGraphic(id, graphic, H, V) {
+function fixValues(object) {
+    let fix = 0;
+    if (object.min.x !== undefined && object.max.x !== undefined) {
+        fix = (object.max.x - object.min.x) * 0.025;
+        object.min.x += -fix;
+        object.max.x += fix;
+        object.length.x = object.max.x - object.min.x;
+    }
+    if (object.min.y !== undefined && object.max.y !== undefined) {
+        fix = (object.max.y - object.min.y) * 0.05;
+        object.min.y += -fix;
+        object.max.y += fix;
+        object.length.y = object.max.y - object.min.y;
+    }
+}
+
+function displayGraphic(H, V) {
     let code = '';
+    const graphic = objects[id-1];
     //code += '<div class="box">\n';
     code += '<div class="title">' + graphic.title + '</div>\n';
     code += '<div class="graphic" id="grph' + id + '">\n';
     code += '<!-- Graphic -->\n';
     code += '<svg width="100%" height="100%">\n';
-    code += '<!-- Grid -->\n';
-    code += '<g stroke="#c8c8c8" stroke-width="1" fill="none">\n';
-    code += '<!-- Frame -->\n';
-    code += '<rect x="0%" y="0%" width="100%" height="100%" stroke-width="2"/>\n';
-    code += '<!-- Horizontal lines -->\n';
-    for (let h = H; h < 100; h += H) {
-        code += '<line x1="0%" y1="' + h + '%" x2="100%" y2="' + h + '%"/>\n';
+    if (graphic.type === 'histogram') {
+        code += displayHistogram();
+    } else if (graphic.type === 'point') {
+        code += displayPoint();
     }
-    code += '<!-- Vertical lines -->\n';
-    for (let v = V; v < 100; v += V) {
-        code += '<line x1="' + v + '%" y1="0%" x2="' + v + '%" y2="100%"/>\n';
-    }
-    code += '</g>';
-    code += displayData(graphic);
     code += '<!-- Pointer -->\n';
     code += '<g class="disable" id="pntr' + id + '" stroke="red" stroke-width="0.5">\n';
     code += '<!-- Horizontal lines -->\n';
@@ -73,51 +84,185 @@ function displayGraphic(id, graphic, H, V) {
     return code;
 }
 
-function displayData(graphic) {
+function displayHistogram() {
+    const graphic = objects[id-1];
     let code = '';
-    if (graphic.type === 'bar') {
-        code += '<!-- Data -->\n';
-        code += '<g stroke="black" stroke-width="0" fill="red">\n';
-        code += '<!-- Bars -->\n';
-        code += '<rect x="24%" y="15%" width="2%" height="35%"/>\n';
-        code += '</g>';
-        code += '<!-- Axis -->\n';
-        code += '<g stroke="black">\n';
-        code += '<!-- Horizontal lines -->\n';
-        code += '<line x1="0%" y1="50%" x2="100%" y2="50%" stroke-width="2"/>\n';
-        code += '<!-- Vertical lines -->\n';
-        code += '<line x1="0%" y1="0%" x2="0%" y2="100%" stroke-width="4"/>\n';
-        code += '</g>';
-    } else if (graphic.type === 'point') {
-        code += '<!-- Axis -->\n';
-        code += '<g stroke="black" stroke-width="2">\n';
-        code += '<!-- Horizontal lines -->\n';
-        code += '<line x1="0%" y1="50%" x2="100%" y2="50%"/>\n';
-        code += '<!-- Vertical lines -->\n';
-        code += '<line x1="50%" y1="0%" x2="50%" y2="100%"/>\n';
-        code += '</g>';
-        code += '<!-- Data -->\n';
-        code += '<g stroke="blue" stroke-width="2" fill="white">\n';
-        code += '<!-- Lines -->\n';
-        code += '<line x1="10%" y1="10%" x2="90%" y2="65%"/>\n';
-        code += '<!-- Points -->\n';
-        code += '<circle cx="10%" cy="10%" r="6"/>\n';
-        code += '<circle cx="90%" cy="65%" r="6"/>\n';
-        code += '</g>';
+    let x0 = 0;
+    let y0 = 0;
+    let height = 0;
+    const step = 100 / ((3 * graphic.data.length) + 1);
+    const width = 2 * step;
+    
+    code += '<!-- Grid -->\n';
+    code += '<g stroke="#c8c8c8" stroke-width="1" fill="none">\n';
+    code += '<!-- Frame -->\n';
+    code += '<rect x="0%" y="0%" width="100%" height="100%" stroke-width="2"/>\n';
+    code += '<!-- Horizontal lines -->\n';
+    if (graphic.min.y <= 0 && graphic.max.y >= 0) {
+        y0 = 100 * (graphic.max.y / graphic.length.y);
+        for (let h = y0 - (Math.round(y0 / 10) * 10); h < 100; h += 10) {
+            code += '<line x1="0%" y1="' + h + '%" x2="100%" y2="' + h + '%"/>\n';
+        }
+    } else {
+        for (let h = 10; h < 100; h += 10) {
+            code += '<line x1="0%" y1="' + h + '%" x2="100%" y2="' + h + '%"/>\n';
+        }
     }
-    //for (var i = 0; i < graphic.data.length; i++) {}
+    code += '</g>';
+    
+    code += '<!-- Data -->\n';
+    code += '<g stroke="black" stroke-width="0" fill="red">\n';
+    code += '<!-- Bars -->\n';
+    x0 = step;
+    for (let i = 0; i < graphic.data.length; i++) {
+        if (graphic.min.y <= 0 && graphic.max.y >= 0) {
+            if (graphic.data[i].y > 0) {
+                y0 = 100 - (100 * (graphic.length.y - graphic.max.y + graphic.data[i].y) / graphic.length.y);
+                height = 100 * (graphic.data[i].y / graphic.length.y);
+                code += '<rect x="' + x0 + '%" y="' + y0 + '%" width="' + width + '%" height="' + height + '%"/>\n';
+            } else if (graphic.data[i].y < 0) {
+                y0 = 100 * (graphic.max.y / graphic.length.y);
+                height = 100 * (Math.abs(graphic.data[i].y) / graphic.length.y);
+                code += '<rect x="' + x0 + '%" y="' + y0 + '%" width="' + width + '%" height="' + height + '%"/>\n';
+            }
+        } else if (graphic.data[i].y > 0) {
+            height = 100 * (graphic.length.y - graphic.max.y + graphic.data[i].y) / graphic.length.y;
+            y0 = 100 - height;
+            code += '<rect x="' + x0 + '%" y="' + y0 + '%" width="' + width + '%" height="' + height + '%"/>\n';
+        } else if (graphic.data[i].y < 0) {
+            y0 = 100 * (graphic.data[i].y - graphic.min.y) / graphic.length.y;
+            height = 100 - y0;
+            code += '<rect x="' + x0 + '%" y="0%" width="' + width + '%" height="' + height + '%"/>\n';
+        }
+        x0 += width + step;
+    }
+    code += '</g>';
+    
+    code += '<!-- Axis -->\n';
+    code += '<g stroke="black">\n';
+    code += '<!-- Horizontal lines -->\n';
+    if (graphic.min.y <= 0 && graphic.max.y >= 0) {
+        y0 = 100 * (graphic.max.y / graphic.length.y);
+        code += '<line x1="0%" y1="' + y0 + '%" x2="100%" y2="' + y0 + '%" stroke-width="2"/>\n';
+    } else if (graphic.min.y > 0) {
+        code += '<line x1="0%" y1="100%" x2="100%" y2="100%" stroke-width="4"/>\n';
+    } else if (graphic.max.y < 0) {
+        code += '<line x1="0%" y1="0%" x2="100%" y2="0%" stroke-width="4"/>\n';
+    }
+    code += '<!-- Vertical lines -->\n';
+    code += '<line x1="0%" y1="0%" x2="0%" y2="100%" stroke-width="4"/>\n';
+    code += '</g>';
+    return code;
+}
+
+function displayPoint() {
+    let code = '';
+    let x1 = 0;
+    let x2 = 0;
+    let y1 = 0;
+    let y2 = 0;
+    const graphic = objects[id-1];
+    
+    code += '<!-- Grid -->\n';
+    code += '<g stroke="#c8c8c8" stroke-width="1" fill="none">\n';
+    code += '<!-- Frame -->\n';
+    code += '<rect x="0%" y="0%" width="100%" height="100%" stroke-width="2"/>\n';
+    code += '<!-- Horizontal lines -->\n';
+    if (graphic.min.y <= 0 && graphic.max.y >= 0) {
+        y1 = 100 * (graphic.max.y / graphic.length.y);
+        for (let h = y1 - (Math.round(y1 / 10) * 10); h < 100; h += 10) {
+            code += '<line x1="0%" y1="' + h + '%" x2="100%" y2="' + h + '%"/>\n';
+        }
+    } else {
+        for (let h = 10; h < 100; h += 10) {
+            code += '<line x1="0%" y1="' + h + '%" x2="100%" y2="' + h + '%"/>\n';
+        }
+    }
+    code += '<!-- Vertical lines -->\n';
+    if (graphic.min.x <= 0 && graphic.max.x >= 0) {
+        x1 = 100 * (Math.abs(graphic.min.x) / graphic.length.x);
+        for (let v = x1 - (Math.round(x1 / 10) * 10); v < 100; v += 5) {
+            code += '<line x1="' + v + '%" y1="0%" x2="' + v + '%" y2="100%"/>\n';
+        }
+    } else {
+        for (let v = 5; v < 100; v += 5) {
+            code += '<line x1="' + v + '%" y1="0%" x2="' + v + '%" y2="100%"/>\n';
+        }
+    }
+    code += '</g>';
+    
+    code += '<!-- Axis -->\n';
+    code += '<g stroke="black">\n';
+    code += '<!-- Horizontal lines -->\n';
+    if (graphic.min.y <= 0 && graphic.max.y >= 0) {
+        y1 = 100 * (graphic.max.y / graphic.length.y);
+        code += '<line x1="0%" y1="' + y1 + '%" x2="100%" y2="' + y1 + '%" stroke-width="2"/>\n';
+    } else if (graphic.min.y > 0) {
+        code += '<line x1="0%" y1="100%" x2="100%" y2="100%" stroke-width="4"/>\n';
+    } else if (graphic.max.y < 0) {
+        code += '<line x1="0%" y1="0%" x2="100%" y2="0%" stroke-width="4"/>\n';
+    }
+    code += '<!-- Vertical lines -->\n';
+    if (graphic.min.x <= 0 && graphic.max.x >= 0) {
+        x1 = 100 * (Math.abs(graphic.min.x) / graphic.length.x);
+        code += '<line x1="' + x1 + '%" y1="0%" x2="' + x1 + '%" y2="100%" stroke-width="2"/>\n';
+    } else if (graphic.min.x > 0) {
+        code += '<line x1="0%" y1="0%" x2="0%" y2="100%" stroke-width="4"/>\n';
+    } else if (graphic.max.x < 0) {
+        code += '<line x1="100%" y1="0%" x2="100%" y2="100%" stroke-width="4"/>\n';
+    }
+    code += '</g>';
+    
+    code += '<!-- Data -->\n';
+    code += '<g stroke="blue" stroke-width="2" fill="white">\n';
+    code += '<!-- Lines & Points -->\n';
+    for (let i = 0; i < graphic.data.length; i++) {
+        if (i === 0) {
+            if (graphic.data[i].x > 0) {
+                x1 = 100 * (graphic.length.x - graphic.max.x + graphic.data[i].x) / graphic.length.x;
+            } else {
+                x1 = 100 * (graphic.data[i].x - graphic.min.x) / graphic.length.x;
+            }
+            if (graphic.data[i].y > 0) {
+                y1 = 100 - (100 * (graphic.length.y - graphic.max.y + graphic.data[i].y) / graphic.length.y);
+            } else {
+                y1 = 100 - (100 * (graphic.data[i].y - graphic.min.y) / graphic.length.y);
+            }
+        } else {
+            if (graphic.data[i].x > 0) {
+                x2 = 100 * (graphic.length.x - graphic.max.x + graphic.data[i].x) / graphic.length.x;
+            } else {
+                x2 = 100 * (graphic.data[i].x - graphic.min.x) / graphic.length.x;
+            }
+            if (graphic.data[i].y > 0) {
+                y2 = 100 - (100 * (graphic.length.y - graphic.max.y + graphic.data[i].y) / graphic.length.y);
+            } else {
+                y2 = 100 - (100 * (graphic.data[i].y - graphic.min.y) / graphic.length.y);
+            }
+            code += '<line x1="' + x1 + '%" y1="' + y1 + '%" x2="' + x2 + '%" y2="' + y2 + '%"/>\n';
+            if (i === 1) {
+                code += '<circle cx="' + x1 + '%" cy="' + y1 + '%" r="6"/>\n';
+            }
+            code += '<circle cx="' + x2 + '%" cy="' + y2 + '%" r="6"/>\n';
+            x1 = x2;
+            y1 = y2;
+        }
+    }
+    code += '</g>';
     return code;
 }
 
 document.addEventListener('mousemove', (mouse) => {
     const ids = document.getElementsByClassName('graphic').length;
-    for (var i = 1; i <= ids; i++) {
-        pointer(i, mouse.clientX, mouse.clientY);
+    for (let i = 0; i < ids; i++) {
+        id = i+1;
+        pointer(mouse.clientX, mouse.clientY);
     }
 });
 
-function pointer(id, mouseX, mouseY) {
-    const roundValue = 1000;
+function pointer(mouseX, mouseY) {
+    const roundValue = 100;
+    const graphic = objects[id-1];
     const graph = document.getElementById('grph' + id);
     const pointer = document.getElementById('pntr' + id);
     const pointerH = document.getElementById('hz-pntr' + id);
@@ -129,7 +274,11 @@ function pointer(id, mouseX, mouseY) {
         pointer.classList.remove('disable');
         pointerH.outerHTML = '<line id="hz-pntr' + id + '" x1="0%" y1="' + (y0 / 300) * 100 + '%" x2="100%" y2="' + (y0 / 300) * 100 + '%"/>';
         pointerV.outerHTML = '<line id="vt-pntr' + id + '" x1="' + (x0 / (0.96 * graph.offsetWidth)) * 100 + '%" y1="0%" x2="' + (x0 / (0.96 * graph.offsetWidth)) * 100 + '%" y2="100%"/>';
-        text.innerHTML = '{' + (Math.round((x0 / (0.96 * graph.offsetWidth)) * roundValue) / roundValue) + ', ' + (Math.round((y0 / 300) *roundValue) / roundValue) + '}';
+        if (graphic.type === 'histogram') {
+            text.innerHTML = '{' + Math.round(graphic.min.x + (graphic.length.x * x0) / (0.96 * graph.offsetWidth)) + ', ' + (Math.round((graphic.max.y - (graphic.length.y * y0 / 300)) *roundValue) / roundValue) + '}';
+        } else {
+            text.innerHTML = '{' + (Math.round((graphic.min.x + (graphic.length.x * x0) / (0.96 * graph.offsetWidth)) * roundValue) / roundValue) + ', ' + (Math.round((graphic.max.y - (graphic.length.y * y0 / 300)) *roundValue) / roundValue) + '}';
+        }
     } else if (! pointer.classList.contains('disable')) {
         pointer.classList.add('disable');
     }
